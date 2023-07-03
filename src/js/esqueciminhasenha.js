3c19a80b93e7fcff472e55ef3b462ffb771e82e9
Lorena <89883311+lorenalopess@users.noreply.github.com>
2023-07-03 09:01:43 -0300

    Add files via upload

---
+++ src/js/esqueciminhasenha.js
@@ -0,0 +1,21 @@
+window.verificarEmail = function(email) {
+  let getuser = localStorage.getItem("listaUser");
+  let userobj = JSON.parse(getuser);
+  let cont = 0;
+
+  if (userobj && userobj.length) {
+    for (let i = 0; i < userobj.length; i++) {
+      console.log(userobj[i].email);
+
+      if (userobj[i].email === email) {
+        cont = 1;
+      }
+    }
+  }
+
+  if (cont === 1) {
+    alert(`Email de redefinição de senha enviado para: ${email}`);
+  } else {
+    alert("Email inválido");
+  }
+}
--- src/js/login.js
+++ src/js/login.js
@@ -1,63 +1,58 @@
-var object = {
-    usuarios: [
-        {
-            Login:"vgoncalves",
-            Senha: "Teste"
-        },
-        {
-            Login:"vgoncalves2",
-            Senha: "Teste2"
-        },
-        {
-            Login:"vgoncalves3",
-            Senha: "Teste3"
-        },
-        {
-            Login:"usuario",
-            Senha: "1234"
-        },
-    ]
-}
-var tentativas = 0;
+//Contador de login inválidos 
+var contaerro  = 0;
+
+function Login(){
+
+//Guarda login e senha digitado 
+let login = document.getElementById('login').value
+let senha = document.getElementById('senha').value
+
+//Acessa o banco 
+let getuser = localStorage.getItem('listaUser')
+
+//Converte 
+let userobj = JSON.parse(getuser)
+
+console.log(login)
+
+//Comando de repetição comparando login e senha no banco
+for(let i=0;i<userobj.length;i++){
+
+    console.log(userobj[i].email)
+
+    if(userobj[i].email==login && userobj[i].senha==senha){
+        //Va para 
+        window.location.href= "../src/index.html";
+        var cont = 1
+    }
+    }
+    //Alerta de bem vindo ou erro
+    if(cont===1){
+        alert("Bem vindo")
+    }
     
-function Login() {
+    else if(cont!=1){
+        alert("Usuario Invalido")    
+        contaerro++;
+        console.log(contaerro);
         
-        var usuario = document.getElementById('login');
-        var senha = document.getElementById('senha');
-        
-        ++tentativas;
-
-        console.log(tentativas);   
-    
-
-        for(let i=0;i<object.usuarios.length;i++){
-
-            if(usuario.value == object.usuarios[i].Login && senha.value == object.usuarios[i].Senha){
-        
-                window.location.href= "index.html";
-                
-                var cont = 1;  
-            }    
-        }
-        
-        if(cont != 1)
-            
-            alert("Usuario ou senha incorreto");    
-        
-        if(tentativas>3){
-            
-            alert("Tentativas Excedidas");
-            window.location.href = "index3.html";
+            //Caso a tentativa de login seja superior a 3, insere a mensagem de erro 
+            if(contaerro>3){
+                alert("Tentativa Excedida")
+                window.location.href = "../src/esqueceusenha.html";
         }
     }
 
-var ul = document.querySelector('nav ul');
-var menuBtn = document.querySelector('.menu-btn i');
-
-function menuShow() {
-    if (ul.classList.contains('open')) {
-        ul.classList.remove('open');
-    }else{
-        ul.classList.add('open');
-    }
 }
+    //Função para aparecer o menu hamburguer 
+    function hamburguer(){        
+        
+        var ul = document.querySelector('nav ul');
+        var menuBtn = document.querySelector('.menu-btn i');
+        
+        if (ul.classList.contains('open')) {
+            ul.classList.remove('open');
+        }else{
+            ul.classList.add('open');
+        }
+    }
